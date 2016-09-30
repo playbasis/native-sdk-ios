6134c39317271bf43547a83af642b0ce696cd20e
Med <med@smartsoftasia.com>
2016-09-30 14:58:16 +0700

    Added goods group verify API

--- PlaybasisSDK.podspec
+++ PlaybasisSDK.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PlaybasisSDK'
-  s.version          = '0.3.4.1'
+  s.version          = '0.3.4.2'
   s.summary          = 'PlaybasisSDK'
 
 # This description is used to generate tags and improve search results.
--- PlaybasisSDK/Classes/PBApi/PBRedeemApi.swift
+++ PlaybasisSDK/Classes/PBApi/PBRedeemApi.swift
@@ -27,4 +27,12 @@ public class PBRedeemApi: PBBaseApi {
             completionBlock()
             }, failureBlock:failureBlock)
     }
+    
+    public class func redeemVerificationWithGroup(group:String, code:String, pinCode:String, completionBlock:PBEmptyCompletionBlock, failureBlock:PBFailureErrorBlock) {
+        let params = ["pin_code":pinCode, "goods_group":group, "coupon_code":code]
+        PBRestController.request(.POST, endPoint: redeemEndPointWithPath("goodsGroup/verify"), parameters: params, completionBlock: { (apiResponse) in
+            completionBlock()
+            }, failureBlock:failureBlock)
+    }
+
 }
