0a55daa0f472eb64eba7a9e9d74d5a305b022a5a
Mederic PETIT <med@smartsoftasia.com>
2016-10-14 13:07:08 +0700

    Fixed app status

--- PlaybasisSDK/Classes/PBApi/PBSettingApi.swift
+++ PlaybasisSDK/Classes/PBApi/PBSettingApi.swift
@@ -18,8 +18,8 @@ public class PBSettingApi: PBBaseApi {
         PBRestController.request(.GET, endPoint: settingEndPointWithPath("appStatus"), parameters: nil, completionBlock: { (apiResponse) in
             if let json:[String:AnyObject] = apiResponse.parsedJson as? [String:AnyObject] {
                 var appPeriod:PBAppPeriod?
-                if let appPeriod:[String:String] = json["app_period"] as? [String:String] {
-                    appPeriod = PBAppPeriod(appPeriodJson: appPeriod)
+                if let appPeriodJson:[String:String] = json["app_period"] as? [String:String] {
+                    appPeriod = PBAppPeriod(appPeriodJson: appPeriodJson)
                 }
                 completionBlock(appStatus: json["app_status"] as? Bool ?? false, appPeriod: appPeriod)
             }
