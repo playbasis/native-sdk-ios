72355686d271249ba1d04d5ab9ebd5147420f4a0
Mederic PETIT <med@smartsoftasia.com>
2016-10-14 13:00:41 +0700

    Fixed a bug in app status endpoint

--- PlaybasisSDK/Classes/PBApi/PBSettingApi.swift
+++ PlaybasisSDK/Classes/PBApi/PBSettingApi.swift
@@ -16,8 +16,12 @@ public class PBSettingApi: PBBaseApi {
     
     public class func getAppStatusWithCompletionBlock(completionBlock:PBAppStatusCompletionBlock, failureBlock:PBFailureErrorBlock) {
         PBRestController.request(.GET, endPoint: settingEndPointWithPath("appStatus"), parameters: nil, completionBlock: { (apiResponse) in
-            if let json:[String:AnyObject] = apiResponse.parsedJson as? [String:AnyObject], let appPeriod:[String:String] = json["app_period"] as? [String:String] {
-                completionBlock(appStatus: json["app_status"] as? Bool ?? false, appPeriod: PBAppPeriod(appPeriodJson: appPeriod))
+            if let json:[String:AnyObject] = apiResponse.parsedJson as? [String:AnyObject] {
+                var appPeriod:PBAppPeriod?
+                if let appPeriod:[String:String] = json["app_period"] as? [String:String] {
+                    appPeriod = PBAppPeriod(appPeriodJson: appPeriod)
+                }
+                completionBlock(appStatus: json["app_status"] as? Bool ?? false, appPeriod: appPeriod)
             }
             else {
                 failureBlock(error: PBError(message: "Unknown error"))
